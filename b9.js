07ba7800dc1e812f61b484abd121754e52467e71
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-28 20:29:50 -0800

    b9 features

--- b9.js
+++ b9.js
@@ -4,3 +4,30 @@
 // suitable for ages 12
 // Help with the command:
 // find().toArray();
+
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+async function fetchRecordsAge12PriceGreaterThan20() {
+    try {        
+        const database = client.db("productDB"); 
+        const collection = database.collection("products");
+
+        const records = await collection.find({
+            price: { $gt: 20 },
+            age: 12
+        }).toArray();
+
+        console.log("Products with price less than 20:", records);
+    } catch (error) {
+        console.error("Error while fetchng files", error)
+    }
+}
+
+fetchRecordsAge12PriceGreaterThan20();
