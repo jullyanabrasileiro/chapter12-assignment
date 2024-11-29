b0850e4090f19e06127f0e21a410ac3c6c507282
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-28 20:26:36 -0800

    b8 features

--- b8.js
+++ b8.js
@@ -3,3 +3,33 @@
 // the records that are in mongodb that cost between 20 and 40.
 // Help with the command:
 // find().toArray();
+
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+async function fetchRecordsWithPriceBetween20And40() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const database = client.db("productDB");
+        const collection = database.collection("products");
+
+        const records = await collection.find({
+            $gte: 20, $lte: 40
+        }).toArray();
+
+        console.log("Products with price 20:", records);
+
+    } catch (error) {
+        console.error("Error while fetching records:", error);
+    }
+}
+
+fetchRecordsWithPriceBetween20And40();
