2fe1019c3aefb71be2ca9af0fd88fdbd218b5912
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-30 11:50:20 -0800

    b11 settings

--- b11.js
+++ b11.js
@@ -1,3 +1,48 @@
 // Do it yourself 11
 // Open a file named b11.js and write an appropriate code that will update the record
 // that cost 20 to 30 Help with updateOne:
+
+
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function fetchRecordsPriceGreaterThan20Age12() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const database = client.db("productDB"); 
+        const collection = database.collection("products"); 
+       
+        const filter = { price: 20 };
+
+        const update = { $set: { price: 30 } };
+
+        const result = await collection.updateOne(filter, update);
+
+        if (result.modifiedCount > 0) {
+            console.log("Record updated sucessfully");
+        } else {
+            console.log("No matching records found to update.")
+        }
+
+        const records = await collection.find().toArray();
+        
+        console.log("Updated records in the database:");
+
+        records.forEach((record, index) => {
+            console.log(`Record ${index + 1}:`, record);
+        });
+    } catch (err) {
+        console.error("Error:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+fetchRecordsPriceGreaterThan20Age12(); 
