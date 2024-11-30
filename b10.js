b489ee8863a23f58664430c0f3562975d8456193
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-30 11:43:21 -0800

    b10 settings

--- b10.js
+++ b10.js
@@ -3,3 +3,36 @@
 // all the records, limit the number of records to 2.
 // Help with the command:
 // find().toArray();
+
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function fetchRecordsPriceGreaterThan20Age12() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const database = client.db("productDB"); 
+        const collection = database.collection("products"); 
+       
+        const records = await collection.find().limit(2).toArray();
+
+        console.log("Here are the first 2 records:");
+
+        for (let i = 0; i < records.length; i++) {
+            console.log(`Record ${i + 1}:`);
+            console.log(records[i]);
+        }
+    } catch (error) {
+        console.error("Error while fetching files", error);
+    } finally {
+        await client.close(); 
+    }
+}
+
+fetchRecordsPriceGreaterThan20Age12(); 
